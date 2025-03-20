a675f4c23ed41ba073f651747d6424722b51d838
miro <jarbasai@mailfence.com>
2025-03-20 01:44:53 +0000

    fix: split translations
    hopefully gitlocalize will stop hanging

--- scripts/sync_translations.py
+++ scripts/sync_translations.py
@@ -5,6 +5,8 @@ TODO - before PR merge
 import json
 from os.path import dirname
 import os
+from ovos_utils.bracket_expansion import expand_template
+from ovos_utils.list_utils import flatten_list, deduplicate_list
 
 locale = f"{dirname(dirname(__file__))}/locale"
 tx = f"{dirname(dirname(__file__))}/translations"
@@ -23,8 +25,14 @@ for lang in os.listdir(tx):
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = [s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
+                if fid.startswith("/"):
+                    p = f"{locale}/{lang.lower()}{fid}"
+                else:
+                    p = f"{locale}/{lang.lower()}/{fid}"
+                os.makedirs(os.path.dirname(p), exist_ok=True)
                 with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
                     f.write("\n".join(sorted(samples)))
 
@@ -37,8 +45,9 @@ for lang in os.listdir(tx):
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = list(set([s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
                 if fid.startswith("/"):
                     p = f"{locale}/{lang.lower()}{fid}"
                 else:
@@ -52,8 +61,9 @@ for lang in os.listdir(tx):
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = list(set([s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
                 if fid.startswith("/"):
                     p = f"{locale}/{lang.lower()}{fid}"
                 else:
@@ -67,8 +77,9 @@ for lang in os.listdir(tx):
             data = json.load(f)
         for fid, samples in data.items():
             if samples:
-                samples = list(set([s.strip() for s in samples
-                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                samples = deduplicate_list(flatten_list([expand_template(s.strip())
+                                                         for s in samples
+                                                         if s and s.strip() != "[UNUSED]"]))  # s may be None
                 if fid.startswith("/"):
                     p = f"{locale}/{lang.lower()}{fid}"
                 else:
